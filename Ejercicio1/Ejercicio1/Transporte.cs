c432f0ea9fa41e4c843609cb8264d69acd8ac265
Nicolas Montalbano <nicolasmontalbano@hotmail.com>
2020-10-22 15:23:08 -0300

    Ejercicio1

---
+++ Ejercicio1/Ejercicio1/Transporte.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    public abstract class Transporte
+    {
+
+        #region Atributos
+        private int pasajeros;    
+        #endregion
+
+        #region Constructor
+        public Transporte(int pasajeros)
+        {
+            this.pasajeros = pasajeros;
+        }
+        #endregion
+
+        #region Metodos publicos
+        public virtual string Avanzar()
+        {
+            return "Avanzo";
+        }
+
+        public virtual string Detenerse()
+        {
+            return "Me detengo";
+        }
+        #endregion
+
+
+    }
+}
